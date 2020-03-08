c1d83fa1dae5a9d5e6878a713a32760f7bf5ced9
collenirwin <collenirwin@gmail.com>
2020-03-08 00:22:36 -0500

    recent sites up and running

--- newtab/src/components/RecentSites.vue
+++ newtab/src/components/RecentSites.vue
@@ -2,15 +2,35 @@
     <div id="top-sites-bar">
         <ul id="top-sites">
             <li>Top Sites</li>
-            <!--<li v-for="site in topSites">
-                <icon-text iconUrl="site.iconUrl" text="site.title"></icon-text>
-            </li>-->
+            <li v-for="(site, index) in topSites" :key="index">
+                <a :href="site.url" class="bookmark">
+                    <IconText :iconUrl="site.iconUrl" :text="site.title" />
+                </a>
+            </li>
         </ul>
     </div>
 </template>
 
 <script>
+import IconText from './IconText.vue';
+import Node from '../Node';
+
 export default {
-    name: 'RecentSites'
+    name: 'RecentSites',
+    data() {
+        return {
+            topSites: []
+        };
+    },
+    components: {
+        IconText
+    },
+    mounted() {
+        // grab all chrome top sites
+        chrome.topSites.get(sites => {
+            // wrap each site in a Node
+            this.topSites = sites.map(site => new Node(site));
+        });
+    }
 }
 </script>
